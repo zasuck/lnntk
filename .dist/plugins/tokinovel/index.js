5068a0c37f5e2495fe70b084e0e483b136be251a
zasuck <zasuck@gmail.com>
2026-07-24 03:10:25 +0900

    Update index.js

--- .dist/plugins/tokinovel/index.js
+++ .dist/plugins/tokinovel/index.js
@@ -1,41 +1,46 @@
-module.exports = {
-  id: "tokinovel",
-  name: "Toki Novel",
-  icon: "https://raw.githubusercontent.com/github/explore/main/topics/javascript/javascript.png",
-  site: "https://toki31.com",
-  version: "1.0.0",
+class TokiNovel extends Plugin {
+  id = 'tokinovel';
+  name = 'Toki Novel';
+  icon = 'https://raw.githubusercontent.com/github/explore/main/topics/javascript/javascript.png';
+  site = 'https://toki31.com';
+  version = '1.0.0';
 
   async popularNovels(page) {
-    return [
-      {
-        name: "테스트 소설",
-        cover: "https://via.placeholder.com/150",
-        path: "/test"
-      }
-    ];
-  },
+    return {
+      novels: [
+        {
+          name: '테스트 소설',
+          cover: 'https://via.placeholder.com/150',
+          path: '/test'
+        }
+      ],
+      hasNextPage: false
+    };
+  }
 
   async parseNovelAndChapters(novelPath) {
     return {
-      name: "테스트 소설",
-      cover: "https://via.placeholder.com/150",
-      author: "작가",
-      summary: "연결 테스트",
+      name: '테스트 소설',
+      cover: 'https://via.placeholder.com/150',
+      author: '작가',
+      summary: '연결 테스트 소설입니다.',
       chapters: [
         {
-          name: "1화",
-          path: "/chap-1",
-          releaseTime: "2026-07-24"
+          name: '1화',
+          path: '/chap-1',
+          releaseTime: '2026-07-24'
         }
       ]
     };
-  },
+  }
 
   async parseChapter(chapterPath) {
-    return "<p>연결 성공!</p>";
-  },
+    return '<p>연결 성공!</p>';
+  }
 
   async searchNovels(searchTerm, page) {
     return [];
   }
-};
+}
+
+module.exports = new TokiNovel();
