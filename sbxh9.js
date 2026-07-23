d50b77b1b24d1e49e4fc827a966c574890a453a5
zasuck <zasuck@gmail.com>
2026-07-24 01:52:47 +0900

    Update sbxh9.js

--- sbxh9.js
+++ sbxh9.js
@@ -7,7 +7,6 @@ class SBXHPlugin {
     this.version = "1.0.0";
   }
 
-  // 1. 인기/최신 소설 목록 가져오기 (테스트용)
   async popularNovels(page) {
     return [
       {
@@ -18,7 +17,6 @@ class SBXHPlugin {
     ];
   }
 
-  // 2. 소설 상세 정보 & 회차 목록 가져오기
   async parseNovelAndChapters(novelPath) {
     return {
       name: "테스트 소설 1",
@@ -35,16 +33,13 @@ class SBXHPlugin {
     };
   }
 
-  // 3. 본문 내용 가져오기
   async parseChapter(chapterPath) {
     return "LNReader v2 플러그인 연결에 성공했습니다! 정상 작동합니다.";
   }
 
-  // 4. 검색 기능
   async searchNovels(searchTerm, page) {
     return [];
   }
 }
 
-// LNReader v2 등록 방식
 module.exports = new SBXHPlugin();
