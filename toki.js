ca736e8fdd7eea903c7cc936bc7b7419fa0bfdd7
zasuck <zasuck@gmail.com>
2026-07-24 02:15:33 +0900

    Update toki.js

--- toki.js
+++ toki.js
@@ -8,21 +8,33 @@ class TokiPlugin {
   }
 
   async popularNovels(page) {
-    return [];
+    return [
+      {
+        name: "테스트 소설",
+        cover: "https://via.placeholder.com/150",
+        path: "/test"
+      }
+    ];
   }
 
   async parseNovelAndChapters(novelPath) {
     return {
-      name: "",
-      cover: "",
-      author: "",
-      summary: "",
-      chapters: []
+      name: "테스트 소설",
+      cover: "https://via.placeholder.com/150",
+      author: "작가",
+      summary: "연결 테스트",
+      chapters: [
+        {
+          name: "1화",
+          path: "/chap-1",
+          releaseTime: "2026-07-24"
+        }
+      ]
     };
   }
 
   async parseChapter(chapterPath) {
-    return "";
+    return "연결 성공!";
   }
 
   async searchNovels(searchTerm, page) {
